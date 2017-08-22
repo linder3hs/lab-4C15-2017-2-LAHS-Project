e0fb5d307a2bd762fa4733f05608395cc97144a5
Linder Hassinger <linderhassinger00@gmail.com>
2017-08-22 10:37:43 -0500

    Inicio de proyecto

---
+++ lab2ExampleGitHub/src/main/java/com/tecsup/lab2/git/controller/StudentController/StudintController.java
@@ -0,0 +1,13 @@
+package com.tecsup.lab2.git.controller.StudentController;
+
+/**
+ * Hello world!
+ *
+ */
+public class StudintController 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ lab2ExampleGitHub/src/test/java/com/tecsup/lab2/git/controller/StudentController/StudintControllerTest.java
@@ -0,0 +1,38 @@
+package com.tecsup.lab2.git.controller.StudentController;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class StudintControllerTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public StudintControllerTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( StudintControllerTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
